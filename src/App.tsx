1cb93d8578cbdce68ae6910d72b2d8d1ffbe01eb
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-05-02 15:36:52 +0900

    style: 작성 페이지 구조 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import { Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
-import LoginPage from './pages/LoginPage';
-import WelcomePage from './pages/WelcomePage';
-import ExistUserPage from './pages/ExistUserPage';
-import KakaoRedirectPage from './pages/KakaoRedirectPage';
-import MyPage from './pages/MyPage/Mypage';
-import Layout from './components/Layout/Layout';
-import SignUpPage from './pages/SignUpPage';
-
+import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import Layout from "./components/Layout/Layout";
+import ExistUserPage from "./pages/ExistUserPage";
+import KakaoRedirectPage from "./pages/KakaoRedirectPage";
+import LoginPage from "./pages/LoginPage";
+import MyPage from "./pages/MyPage/Mypage";
+import SignUpPage from "./pages/SignUpPage";
+import WelcomePage from "./pages/WelcomePage";
+import WritePage from "./pages/WritePage";
 
 function App() {
   return (
     <AppWrapper>
       <Routes>
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/auth/kakao/callback' element={<KakaoRedirectPage />} />
-        <Route path='/signUp' element={<SignUpPage />} />
-        <Route path='/welcome' element={<WelcomePage />} />
-        <Route path='/existUser' element={<ExistUserPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/auth/kakao/callback" element={<KakaoRedirectPage />} />
+        <Route path="/signUp" element={<SignUpPage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/existUser" element={<ExistUserPage />} />
         {/* 헤더를 포함하는 페이지 */}
-        <Route element={<Layout/>}>
-        <Route path='/mypage' element={<MyPage />} />
+        <Route element={<Layout />}>
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/write" element={<WritePage />} />
         </Route>
       </Routes>
     </AppWrapper>
