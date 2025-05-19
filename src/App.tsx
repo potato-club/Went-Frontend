b8d5372d049c4609fd42edbb00a01ff881589bb4
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-05-19 16:12:20 +0900

    feat: 구글 로그인 id token 발급 로직으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import ExistUserPage from "./pages/ExistUserPage";
 import KakaoRedirectPage from "./pages/KakaoRedirectPage";
 import LoginPage from "./pages/LoginPage";
 import MyPage from "./pages/MyPage/Mypage";
-import SignUpPage from "./pages/SignUpPage";
 import WelcomePage from "./pages/WelcomePage";
 import WritePage from "./pages/WritePage";
+import SignUpPage from "./pages/SignupPage";
 
 function App() {
   return (
